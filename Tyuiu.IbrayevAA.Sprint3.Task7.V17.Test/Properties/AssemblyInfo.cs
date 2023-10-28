139940183fd12b34e3f177a9de4e4be194475ab7
Tyuiu.IbrayevAA@outlook.com <03.05.05.ibraev.abai@gmail.com>
2023-10-29 04:22:51 +0500

    Task7

---
+++ Tyuiu.IbrayevAA.Sprint3.Task7.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IbrayevAA.Sprint3.Task7.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IbrayevAA.Sprint3.Task7.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("616ccd01-fe9f-4e73-b459-bc0f7c53d413")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
